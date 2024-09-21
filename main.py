ffa1140afc9cbac6c910dff95cb9aac98d39c0f0
iamklager <gerhard.klager@gmail.com>
2024-09-21 15:32:24 +0200

    Updated GUI customtkinter

--- main.py
+++ main.py
@@ -1,43 +1,65 @@
-#### Packages
-import tkinter # For the popup window
-import time # For waiting 30min
+import tkinter as tk
+import customtkinter as ctk
+from PIL import Image
+from time import sleep
 
-#### Function to create the popup
-def f_CreatePopup(
-        n_PopupWidth = 640,
-        n_PopupHeight = 240,
-        c_PopupMessage = "\n30min are over!\n\nStand up!\n\nStraighten your back!\n\nWalk a bit!\n\nAnd sit down in PROPER position!\n\nYou shrimp!",
-        c_PopupTitle = "Back Savior",
-        n_WaitingTime = 5 # Time to wait before another popup appears (in seconds)
-    ):
-    ### Dynamic variables
-    c_PopupSize = str(n_PopupWidth) + "x" + str(n_PopupHeight) # Popup window size
-    b_PopupOpen = False # Popup state (True = Open, False = Close)
-    
-    ### Main loop
-    while 1:
-        # Changing the popup state
-        b_PopupOpen = False
+# Theming
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("blue")
+
+# App class
+class PopUpWindow(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # Window config
+        self.title("Back Savior")
+        self.geometry("680x290")
         
-        ## Creating a popup window if none is open
-        if b_PopupOpen == False:
-            # Popup object
-            tk_Popup = tkinter.Tk()
-            tk_Popup.geometry(c_PopupSize)
-            tk_Popup.title(c_PopupTitle)
-            # Annotating a label
-            tk_MessageLabel = tkinter.Label(tk_Popup, text = c_PopupMessage)
-            tk_MessageLabel.pack()
-            # Saving the popup state as True
-            b_PopupOpen = True
+        # GIF setup
+        self.file = "shrimp.gif"
+        self.info = Image.open(self.file)
+        self.frames = self.info.n_frames
+        self.photoimage_objects = []
+        for i in range(self.frames):
+            obj = tk.PhotoImage(file=self.file, format=f"gif -index {i}")
+            self.photoimage_objects.append(obj)
 
-            # Main loop of the tkinter popup object
-            tk_Popup.mainloop()
-            
-            #  Waiting the desired amount of time
-            time.sleep(n_WaitingTime)
+        # GIF frame
+        self.gif_frame = ctk.CTkFrame(master=self, width=270, height=270)
+        self.gif_frame.place(x=400, y=10)
+        self.gif_label = ctk.CTkLabel(master=self.gif_frame, image = "", text="", width=270, height=270)
+        self.animation()
+        self.gif_label.place(x=0, y=0)
 
+        # Text frame
+        txt_frame = ctk.CTkFrame(master=self, width=380, height=220)
+        txt_frame.place(x=10, y=10)
+        txt_label = ctk.CTkLabel(
+            master=txt_frame, width=380, height=220, justify="center",
+            text="\n30min are over!\n\nStand up!\n\nStraighten your back!\n\nWalk a bit!\n\nAnd sit down in PROPER position!\n\nYou shrimp!"
+        )
+        txt_label.place(x=0, y=0)
 
-#### Main program
+        # Done button
+        done_button = ctk.CTkButton(master=self, text="Done", width=380, height=40, command=self.destroy)
+        done_button.place(x=10, y=240)
+
+    # GIF animation function
+    def animation(self, current_frame=0):
+        global loop
+        image = self.photoimage_objects[current_frame]
+        self.gif_label.configure(image = image)
+        current_frame = current_frame + 1
+        if current_frame == self.frames:
+            current_frame = 0
+        loop = self.after(50, lambda: self.animation(current_frame))
+    
+    
+# Main app loop
 if __name__ == "__main__":
-    f_CreatePopup(n_WaitingTime = 30*60)
+    while 1:
+        app = PopUpWindow()
+        app.mainloop()
+        sleep(30*60)
+    
